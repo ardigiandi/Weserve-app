ec63a36baf8388756913229aa204d3991e6351ff
ardigiandi <ardiansyahg9080@gmail.com>
2023-12-24 23:33:16 +0700

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        silver : '#080C2E',
+        grey : '#424560',
+        grey1: '#8D90A8',
+      },
+    },
   },
   plugins: [],
 }
