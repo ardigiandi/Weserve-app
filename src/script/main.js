5f74e7fde483478038c8f90a9cbc65292a64fbba
ardigiandi <ardiansyahg9080@gmail.com>
2023-12-29 00:47:51 +0700

    slicing web

--- src/script/main.js
+++ src/script/main.js
@@ -1,11 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Get the toggle button and navbar links
-    const toggleBtn = document.getElementById("toggleBtn");
-    const navbarLinks = document.getElementById("navbarLinks");
-
-    // Add click event listener to the toggle button
-    toggleBtn.addEventListener("click", function () {
-        // Toggle the 'hidden' class on navbar links
-        navbarLinks.classList.toggle("hidden");
+    const btndropdown = document.getElementById("btn-dropdown");
+    const dropdownmenu = document.getElementById("dropdown-menu");
+  
+    btndropdown.addEventListener("click", function () {
+      dropdownmenu.classList.toggle("hidden");
     });
-});
+  
+    document.addEventListener("click", function (event) {
+      if (
+        !btndropdown.contains(event.target) &&
+        !dropdownmenu.contains(event.target)
+      ) {
+        dropdownmenu.classList.add("hidden");
+      }
+    });
+  });
